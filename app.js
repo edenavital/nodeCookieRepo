34eb6e00c26cc1b650d7f37d6c59afd940d3c68a
Eden Avital <edendor@gmail.com>
2021-07-12 14:10:42 +0300

    hotfix origin

--- app.js
+++ app.js
@@ -9,23 +9,25 @@ const mainRouter = require("./routes/mainRouter");
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
-// Serving static files
-// app.use(express.static(path.join(__dirname, "public")));
 
 // Set security HTTP headers
 // app.use(helmet());
 
 app.use(cors());
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  const origin =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:5000"
+      : "https://node-cookie.herokuapp.com";
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // Development logging
 if (process.env.NODE_ENV === "development") {
