8cabec437e673533f8a7f9a078673a20f01ec9fb
Eden Avital <edendor@gmail.com>
2021-07-12 17:06:13 +0300

    disable httpOnly

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -13,7 +13,7 @@ exports.generateToken = (req, res, next) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
+    // httpOnly: true,
   };
 
   if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
