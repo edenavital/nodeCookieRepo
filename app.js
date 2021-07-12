b0009fe076944a56e08afbb54983117cbaa1489a
Eden Avital <edendor@gmail.com>
2021-07-12 13:39:02 +0300

    url route changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const path = require("path");
+// const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
-const helmet = require("helmet");
+// const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const mainRouter = require("./routes/mainRouter");
@@ -13,19 +13,19 @@ const app = express();
 // app.use(express.static(path.join(__dirname, "public")));
 
 // Set security HTTP headers
-app.use(helmet());
+// app.use(helmet());
 
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 // Development logging
 if (process.env.NODE_ENV === "development") {
@@ -33,8 +33,8 @@ if (process.env.NODE_ENV === "development") {
 }
 
 // Body parser, reading data from body into req.body
-app.use(express.json({ limit: "10kb" }));
-app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Allow to read cookies in server (req.cookies)
 app.use(cookieParser());
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import "./App.css";
-const URL = "http://localhost:5000/api";
+const URL = "/api";
 
 const App = () => {
   const specialReq = async ({ method, data }) => {
