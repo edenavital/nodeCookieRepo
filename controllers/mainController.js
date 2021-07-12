d15d05a700193009aa3a96594f4a75e74df11890
Eden Avital <edendor@gmail.com>
2021-07-12 17:27:18 +0300

    set a cross-site cookie for third-party contexts

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -13,7 +13,8 @@ exports.generateToken = (req, res, next) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    // sameSite: "None",
+    sameSite: "none",
+    secure: true,
     // httpOnly: true,
   };
 
